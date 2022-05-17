9272cb496ab80e7da93b809ac14f246dbc2c5ef2
Elizaveta_Pomaleiko <lizapom00@mail.ru>
2022-05-17 12:52:00 +0700

    update

--- lessons_4-6/carousel.js
+++ lessons_4-6/carousel.js
@@ -1,4 +1,4 @@
-let slides = document.getElementByClassName('custom-carousel-item');
+
 
 let index = 0;
 
@@ -10,21 +10,36 @@ function next(){
 showSlide(++index);
 }
 
-const next_btn = document.getElementByClassName('custom-carousel-control-next') [0];
-const prev_btn = document.getElementByClassName('custom-carousel-control-prev')[0];
+function currentSlide(i) {
+	showSlide(index = i);
+}
 
+const next_btn = document.getElementsByClassName('custon-carousel-control-next')[0];
+const prev_btn = document.getElementsByClassName('custom-carousel-control-prev')[0];
 
-//console.log(next_btn, prev_btn);
-
-next_btn.addEventListener('click'.next);
-prev_btn.addEventListener('click'.next);
+next_btn.addEventListener('click', next);
+prev_btn.addEventListener('click', prev);
 
 function showSlide(i) {
-	let slides = document.getElementByClassName('custom-carousel-item');
+	let slides = document.getElementsByClassName('custom-carousel-item');
+	let length = slides.length - 1;
 
-let slide = slides[i];
+	if (i < 1) {
+		index = length;
+	}
 
-slides[i-1].classList.remove('active');
+	if (i > length){
+		index = 0;
+	}
+
+	
+
+let slide = slides[index];
+
+for (let element of slides) {
+	element.classList.remove('active');
+}
+ 
 slide.classList.add('active');
 
 console.log(i, slide.classList);
